97a1ea48a64926c1dd862bc5e24b185db103c9d2
Mohammad Tavassoli <MohammadTavassoliOrg@gmail.com>
2023-09-17 10:52:51 +0330

    Import config module as a global module and load configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import configuration from 'config/configuration';
+import databaseConfig from 'config/database.config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    load: [configuration, databaseConfig]
+  }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
