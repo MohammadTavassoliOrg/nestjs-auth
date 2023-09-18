5a62f62f2dcac9d58b312f97e34aa2b830ebe3c2
Mohammad Tavassoli <MohammadTavassoliOrg@gmail.com>
2023-09-18 12:10:19 +0330

    Change env validation EnvironmentVariables class, Add new env variables

--- src/helpers/env.validation.ts
+++ src/helpers/env.validation.ts
@@ -1,15 +1,32 @@
 import { plainToInstance } from 'class-transformer';
-import { IsNumber, IsString, validateSync } from 'class-validator'
+import { IsNotEmpty, IsNumber, IsString, validateSync } from 'class-validator'
 
 class EnvironmentVariables {
     @IsNumber()
+    @IsNotEmpty()
     PORT: number;
 
     @IsString() 
+    @IsNotEmpty()
     DB_NAME: string;
 
     @IsNumber()
+    @IsNotEmpty()
     DB_PORT: number = 3306;
+
+    @IsString()
+    DB_HOST: string = 'localhost';
+
+    @IsString()
+    DB_TYPE: string = 'mysql';
+    
+    @IsString()
+    @IsNotEmpty()
+    DB_USERNAME: string;
+
+    @IsString()
+    @IsNotEmpty()
+    DB_PASSWORD: string;
 }
  
 export function validate(config: Record<string, unknown>) {
@@ -22,7 +39,6 @@ export function validate(config: Record<string, unknown>) {
     if (errors.length > 0) {
         throw new Error(errors.toString());
     } 
-    console.log(validatedConfig);
     
     return validatedConfig;
 }
