fc29886e3b464f39ee0e1abee64258c78de3868c
Mohammad Tavassoli <MohammadTavassoliOrg@gmail.com>
2023-09-18 12:01:00 +0330

    Complete database configs

--- config/database.config.ts
+++ config/database.config.ts
@@ -4,4 +4,7 @@ export default registerAs('database', () => ({
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 3306,
     name: process.env.DB_NAME || 'auth',
-}))
+    type: process.env.DB_TYPE || 'mysql',
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || 'mohammad',
+}));
