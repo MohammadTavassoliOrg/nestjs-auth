3a9c567166b024a4520491a8ed42014dedbb1471
Mohammad Tavassoli <MohammadTavassoliOrg@gmail.com>
2023-09-18 12:08:35 +0330

    Add typeorm config service

---
+++ src/database/typeorm-config.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: this.configService.get('database.type'),
+      host: this.configService.get('database.host'),
+      port: this.configService.get('database.port'),
+      username: this.configService.get('database.username'),
+      password: this.configService.get('database.password'),
+      database: this.configService.get('database.name'),
+      synchronize: true,
+      entities: [User]
+    } as TypeOrmModuleOptions;
+  }
+}
