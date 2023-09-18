bd4be2f711310d358752c8127531d73201c293c9
Mohammad Tavassoli <MohammadTavassoliOrg@gmail.com>
2023-09-18 12:07:02 +0330

    Import users, auth and typeorm module, Addd db configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
 import { validate } from './helpers/env.validation';
 import configuration from 'config/configuration';
 import databaseConfig from 'config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './database/typeorm-config.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    validate,
-    isGlobal: true,
-    load: [configuration, databaseConfig]
-  }), UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      validate,
+      isGlobal: true,
+      load: [configuration, databaseConfig]
+    }), TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }), AuthModule, UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
