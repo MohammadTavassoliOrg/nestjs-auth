04d66fcfbdea271ec8e745ad30ef3f01b2d1908f
Mohammad Tavassoli <MohammadTavassoliOrg@gmail.com>
2023-09-24 06:18:32 +0330

    Add update user dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @IsOptional()
+    @IsBoolean()
+    isDeleted: boolean;
+
+    @IsOptional()
+    @IsString()
+    refresh_token: string;
+
+    @IsOptional()
+    @IsString()
+    access_token_hash: string;
+}
