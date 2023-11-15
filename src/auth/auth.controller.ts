bd5240fc72acc0ebab5cb49d9ae0be394744d3ff
Mohammad Tavassoli <MohammadTavassoliOrg@gmail.com>
2023-11-15 10:00:17 +0330

    Complete login method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,27 +1,52 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, HttpCode, HttpStatus, Post, Req, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { SignInDto } from './dto/signin.dto';
 import { SignUpDto } from './dto/signup.dto';
 import { UsersService } from 'src/users/users.service';
 import { AuthErrors } from './enums';
 import { ConfigService } from '@nestjs/config';
 import * as bcrypt from 'bcrypt';
+import { SignInDto } from './dto/signin.dto';
+import { HashManager } from '../helpers/hash-manager.helper';
+import { LogInInterface } from './interface/login.interface';
 
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly authService: AuthService, 
     private readonly usersService: UsersService, 
-    private readonly configService: ConfigService
+    private readonly configService: ConfigService,
   ) {}
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
+  async login(@Body() signInDto: SignInDto): Promise<LogInInterface> {
+    const userFound = await this.usersService.findUserByEmail(signInDto.email);
+    if (!userFound)
+      throw new UnauthorizedException(AuthErrors.INVALID_CREDINTIALS);
+
+    if (!new HashManager(this.configService).compareSync(signInDto.password, userFound.password)) {
+      throw new BadRequestException(AuthErrors.INVALID_CREDINTIALS);
+    }
+
+    const { refresh_token, refresh_token_generate_date } = this.authService.generateRefreshToken();
+    const access_token = await this.authService.generateAccessToken(userFound.id);
+    const access_token_hash = await bcrypt.hashSync(access_token, +this.configService.get('app.salt'));
+
+    const newTokens = {
+      refresh_token, refresh_token_generate_date, access_token_hash
+    }
+    await this.usersService.updateUserTokens(userFound.id, newTokens);
+
+    return {
+      access_token,
+      refresh_token,
+    }
+  }
 
   @HttpCode(HttpStatus.OK)
   @Post('signup')
-  async signUp(@Body() signUpDto: SignUpDto) {
-    const userFound = await this.usersService.findUserByEmail(signUpDto.email);
+  async signUp(@Body() signUpDto: SignUpDto): Promise<LogInInterface> {
+    const userFound = await this.usersService.isExistsUser(signUpDto.email);
     if (userFound)
       throw new UnauthorizedException(AuthErrors.INVALID_CREDINTIALS);
 
