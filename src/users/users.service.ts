7f5b208cd9ee5031f1dc193a2dae792fc48783a4
Mohammad Tavassoli <MohammadTavassoliOrg@gmail.com>
2023-11-15 07:11:31 +0330

    Add hashing user password method and develop create user method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,39 +1,56 @@
+import { ConfigService } from '@nestjs/config';
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+    private configService: ConfigService
   ) {}
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  async createUser(
+    createUserDto: CreateUserDto, refresh_token: string, 
+    refresh_token_generate_date: Date
+  ) {
+
+    const password = await this.hashUserPassword(createUserDto.password);
+    const user = await this.usersRepository.save({
+      ...createUserDto, password,
+      refresh_token, refresh_token_generate_date
+    });
+    return user;
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async hashUserPassword(password: string): Promise<string> {
+    const hash = await bcrypt.hashSync(password, +this.configService.get('app.salt'));
+    return hash;
   }
 
-  async findOneByEmail(email: string): Promise<User | null> {
-    const user = await this.usersRepository.findOne({where: { email }});
+  async addUserTokenHash(id: number, access_token_hash: string) {
+    const user = await this.usersRepository.update(id, { access_token_hash });
+    return user;
+  }
+
+  async findUserByEmail(email: string): Promise<User | null> {
+    const user = await this.usersRepository.findOne({where: { email, isDeleted: false }});
     return user;
   }
 
   async findOneByRefreshToken(refresh_token: string): Promise<User | null> {
-    const user = await this.usersRepository.findOne({where: { refresh_token }});
+    const user = await this.usersRepository.findOne({where: { refresh_token, isDeleted: false }});
     return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async updateUserTokens(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.usersRepository.save({...updateUserDto, id});
+    return user;
   }
 }
