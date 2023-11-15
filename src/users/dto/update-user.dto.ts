7eee2dc4ab6929c3c59c95fc881a2b367683b6c2
Mohammad Tavassoli <MohammadTavassoliOrg@gmail.com>
2023-11-15 07:18:38 +0330

    Add refresh_token_generate_date key

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsBoolean, IsOptional, IsString } from 'class-validator';
+import { IsBoolean, IsDate, IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
     @IsOptional()
@@ -14,4 +14,8 @@ export class UpdateUserDto extends PartialType(CreateUserDto) {
     @IsOptional()
     @IsString()
     access_token_hash: string;
+
+    @IsOptional()
+    @IsDate()
+    refresh_token_generate_date: Date;
 }
