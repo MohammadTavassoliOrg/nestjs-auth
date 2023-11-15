f77a19f32f6d14e6ff439a306adf306bf9985819
Mohammad Tavassoli <MohammadTavassoliOrg@gmail.com>
2023-11-15 12:26:02 +0330

    Change helpers location to common

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { validate } from './helpers/env.validation';
+import { validate } from './common/helpers/env.validation';
 import configuration from 'config/app.config';
 import databaseConfig from 'config/database.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
